a77e0d9de9d9081ced01c9bd396e19ec638e3e3b
Nickolas Gupton <CorruptComputer@protonmail.com>
2025-04-14 10:50:53 -0500

    Update configuration.nix

--- armadillo/configuration.nix
+++ armadillo/configuration.nix
@@ -12,28 +12,24 @@
       efi.canTouchEfiVariables = true;
     };
 
-    initrd = {
-      luks.devices."luks-afbdebe4-1342-4dca-b0b3-28e9ee8da74d".device = "/dev/disk/by-uuid/afbdebe4-1342-4dca-b0b3-28e9ee8da74d";
-    };
-
     kernelPackages = pkgs.linuxPackages_latest;
     kernelModules = [
       "amdgpu"
-      "v4l2loopback"
+      #"v4l2loopback"
     ];
-    extraModulePackages = with config.boot.kernelPackages; [
-      v4l2loopback.out
-    ];
-    extraModprobeConfig = ''
-      options v4l2loopback exclusive_caps=1 card_label="Virtual Camera"
-    '';
+    #extraModulePackages = with config.boot.kernelPackages; [
+      #v4l2loopback.out
+    #];
+    #extraModprobeConfig = ''
+    #  options v4l2loopback exclusive_caps=1 card_label="Virtual Camera"
+    #'';
   };
 
   networking = {
     hostName = "armadillo";
     networkmanager.enable = true;
   };
-  
+
   time = {
     timeZone = "America/Chicago";
   };
@@ -54,13 +50,19 @@
   };
 
   services = {
+    displayManager.defaultSession = "plasma";
+    displayManager.sddm.enable = true;
+    displayManager.sddm.wayland.enable = true;
+    desktopManager.plasma6.enable = true;
+
     xserver = {
       enable = true;
-      displayManager.gdm.enable = true;
-      desktopManager.gnome.enable = true;
 
-      layout = "us";
-      xkbVariant = "";
+
+      xkb = {
+        layout = "us";
+        variant = "";
+      };
 
       videoDrivers = [
         "amdgpu"
@@ -69,11 +71,6 @@
       excludePackages = with pkgs; [
         xterm
       ];
-    };
-
-    gnome = {
-      games.enable = true;
-      core-developer-tools.enable = true;
     };
 
     fwupd.enable = true;
@@ -87,35 +84,33 @@
       jack.enable = true;
     };
 
-    udev.packages = with pkgs; [
-      gnome.gnome-settings-daemon
-    ];
-
-    flatpak.enable = true;
+    #flatpak.enable = true;
   };
-
-  sound.enable = true;
 
   hardware = {
     pulseaudio = {
       enable = false;
     };
 
-    opengl = {
-      driSupport = true;
-      driSupport32Bit = true;
+    graphics = {
+        enable = true;
+        enable32Bit = true;
 
-      extraPackages = with pkgs; [
-        amdvlk
-        libva
-        rocm-opencl-icd
-      ];
+        extraPackages = with pkgs; [
+          amdvlk
+          libva
+        ];
 
-      extraPackages32 = with pkgs; [
-        driversi686Linux.amdvlk
-      ];
+        extraPackages32 = with pkgs; [
+          driversi686Linux.amdvlk
+        ];
     };
-    
+
+    amdgpu.amdvlk = {
+        enable = true;
+        support32Bit.enable = true;
+    };
+
     bluetooth = {
       enable = true;
       powerOnBoot = true;
@@ -125,53 +120,46 @@
 
   security.rtkit.enable = true;
 
-  # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.ngupton = {
     isNormalUser = true;
-    description = "Nickolas Gupton";
+    description = "Nick";
     extraGroups = [ "networkmanager" "wheel" ];
-    packages = with pkgs; [
-      # Should be installed via flatpak, things like:
-      # firefox
-      # steam
-      # discord
-      # gitkraken
-      # etc.
+  };
+
+  programs = {
+    steam = {
+      enable = true;
+      remotePlay.openFirewall = true;
+      dedicatedServer.openFirewall = true;
+      localNetworkGameTransfers.openFirewall = true;
+    };
+
+    dconf.enable = true;
+  };
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment = {
+    systemPackages = with pkgs; [
+      vim
+      wget
+      vscode
+      flatpak-builder
+      tailscale
+      pcloud
+      discord-canary
     ];
   };
 
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
 
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
-  environment = {
-    systemPackages = with pkgs; [
-      linuxKernel.packages.linux_latest_libre.v4l2loopback
-      vim
-      wget
-      vscode
-      gnomeExtensions.appindicator
-      gnome.gnome-software
-      gnome3.gnome-tweaks
-      flatpak-builder
-      tailscale
-      pcloud
-    ];
-  };
 
-  system = {
-    # This value determines the NixOS release from which the default
-    # settings for stateful data, like file locations and database versions
-    # on your system were taken. It‘s perfectly fine and recommended to leave
-    # this value at the release version of the first install of this system.
-    # Before changing this value read the documentation for this option
-    # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-    stateVersion = "23.11"; # Did you read the comment?
-
-    autoUpgrade = {
-      enable = true;
-      allowReboot = false;
-    };
-  };
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "24.11"; # Did you read the comment?
 }
