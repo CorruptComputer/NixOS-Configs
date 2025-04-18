b88a30aa966c1b383423b8d17873cc2c868312b5
Nickolas Gupton <CorruptComputer@protonmail.com>
2025-04-18 00:53:25 -0500

    Update configuration.nix

--- armadillo/configuration.nix
+++ armadillo/configuration.nix
@@ -201,13 +201,29 @@
   };
 
   nixpkgs.config.allowUnfree = true;
+  nix = {
+    optimise.automatic = true;
+    gc = {
+      automatic = true;
+      dates = "weekly";
+      options = "--delete-older-than 30d";
+    };
+  };
 
 
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "24.11"; # Did you read the comment?
+
+  system = {
+    autoUpgrade = {
+      enable = true;
+      allowReboot = false;
+    };
+
+    # This value determines the NixOS release from which the default
+    # settings for stateful data, like file locations and database versions
+    # on your system were taken. It‘s perfectly fine and recommended to leave
+    # this value at the release version of the first install of this system.
+    # Before changing this value read the documentation for this option
+    # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+    stateVersion = "24.11"; # Did you read the comment?
+  };
 }
